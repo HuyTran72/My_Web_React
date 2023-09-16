80d7ec9a4b4bfd7bf04111b406bf2540ee710e09
Huy Tran <baohuy82.th@gmail.com>
2023-09-15 21:26:04 -0700

    commit

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assets/image.png';
+import avatar from '../../assets/IMG_2035.png';
 import { Link } from 'react-scroll';
 import btnImg from '../../assets/hireme.png';
 
@@ -8,15 +8,18 @@ import btnImg from '../../assets/hireme.png';
 const Intro = () => {
     return (
         <section id="intro">
+            <img src={avatar} alt="Profile" className="avatar" />
             <div className="introContent">
                 <span className="hello">Hello,</span>
-                <span className="introText"> I'm Huy Tran.</span>
+                <span className="introText"> My name is <span className='introName'> Huy Tran.</span> <br />I'm Junior in Software Engineering at San Jose State University</span>
                 <p className="introPara">I'm very professional in develop app.</p>
-                <Link><button className="but"><img src={btnImg} alt="Hire me" />Hire me</button></Link>
+                <Link><button className="butn"><img src={btnImg} alt="Hire me" className='btnImg' />Hire me</button></Link>
             </div>
-            <img src="" alt="" className="bg" />
+            
         </section>
     )
 }
 
 export default Intro
+
+
