8be1c8ef71c01359033bd1d7815cccd9a5544df0
Huy Tran <baohuy82.th@gmail.com>
2023-09-16 00:35:24 -0700

    commit

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import './skills.css';
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+import AppDesign from '../../assets/app-design.png';
 
 const Projects = () => {
   return (
