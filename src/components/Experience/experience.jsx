d53832824c22529e60a7650b0d32efb6fd9c1da8
Huy Tran <baohuy82.th@gmail.com>
2023-09-18 20:07:22 -0700

    commit

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -4,35 +4,57 @@ import MUI from '@mui/material';
 import { Container, Row, Col } from 'react-bootstrap';
 import TutorImg from '../../assets/tutor.png';
 import ResearchImg from '../../assets/research.png';
+import TeamLeadImg from '../../assets/TeamLeader.jpg';
 
 const Experience = () => {
   return (
     <Container>
         <Row>
-            <section id='experience'>
-            <h1 className="expTitle">PERSONAL WORK EXPERIENCE</h1>
-            <span className="expDesc">All my projects show my skills practing with code druring my study in school.</span>
-            <Col sm='4'>
+            <Col>
+                <section id='experience'>
+                    <h2 className="expTitle">WORK EXPERIENCE</h2>
                     <div className="expBars">
-                        <div className="expBar">
-                            <img src={TutorImg} alt="Tutor" className='TutorImg'/>
-                            <div>
-                                <h3>Computer and Math Tutor</h3>
-                                <ul>
-                                    <li>Worked in the Tutoring Center to help over 50 students in Calculus I, II, Discrete Math, Linear Algebra, Differential Equations, Statistics, Java Programming, Python, and Data Structure</li>
-                                    <li>Help</li>
-                                </ul>
-                            </div>
+                        <div className='expBar'>
+                            <img src={TutorImg} alt="work1" className="expImg" />
+                                <div>
+                                    <h5>Computer and Math Tutor</h5>
+                                        <ul>
+                                            <li>Worked in the Tutoring Center to assisted over 100 students in Math, Java, Data and Algorithm</li>
+                                            <li>Using CSS</li>
+                                            <li>By React</li>
+                                        </ul>
+                                </div>
                         </div>
+                
+
+                        <div className='expBar'>
+                            <img src={ResearchImg} alt="exp2" className="expImg" />
+                            <div>
+                                <h5>Research Assistant</h5>
+                                <ul>
+                                    <li>Collaborated with a prefessor to research and build scalable templates in Lorem Ipsum</li>
+                                    <li>Provided training sessions for classmates to use effectively utilized the data program</li>
+                                    <li>Keep track of each student's progress, sent reminders to ensure they were working on their goals</li>
+                                </ul>
+                            </div> 
+                        </div>
+                 
+            
+                        <div className='expBar'>
+                            <img src={TeamLeadImg} alt="exp3" className="expImg" />
+                            <div>
+                                <h5>Peer Team Learning Leader</h5>
+                                <ul>
+                                    <li>Engaged classmates to work in a project group of five</li>
+                                    <li>Help them to review the difficult lessons and problems to enhance their matter knowledge, study skills, and course success</li>
+                                    <li>Build, manage, and develop the Lovelace Cupcakes project by using Java</li>
+                                </ul>
+                            </div>  
+                        </div>
+                    
                     </div>
+                </section>
             </Col>
-
-            <Col sm='4'>
-                    <div className="expBar"></div>
-                    <img src={ResearchImg} alt="" className='ResearchImg'/>
-            </Col>
-            </section>
-
         </Row>
     </Container>
       
--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -12,56 +12,49 @@ const Projects = () => {
     <Container>
         <Row>
             <Col>
-            <section id='projects'>
-        <h2 className="projectTitle">MY PROJECTS</h2>
-        <span className="projectDesc">All my projects show my skills practing with code druring my study in school.</span>
-        <div className="projectBars">
-            <div className='projectBar'>
-            <   img src={project1} alt="project1" className="projectImg" />
-                <div>
-                    <h5> <a href="https://huytran72.github.io/My_Web_React/">Personal Website </a>| HTLM, CSS, JavaScrip, Reactjs |<a href="https://github.com/HuyTran72/My_Web_React"> GitHub</a> </h5>
-                    <ul>
-                        <li>
-                            Using HTML
-                        </li>
-                        <li>
-                            Using CSS
-                        </li>
-                        <li>
-                            By React
-                        </li>
-                    </ul>
-                </div>
-            </div>
+                <section id='projects'>
+                    <h2 className="projectTitle">MY PROJECTS</h2>
+                    <span className="projectDesc">All my projects show my skills practing with code druring my study in school.</span>
+                    <div className="projectBars">
+                        <div className='projectBar'>
+                            <img src={project1} alt="project1" className="projectImg" />
+                                <div>
+                                    <h5> <a href="https://huytran72.github.io/My_Web_React/">Personal Website </a>| HTLM, CSS, JavaScrip, Reactjs |<a href="https://github.com/HuyTran72/My_Web_React"> GitHub</a> </h5>
+                                        <ul>
+                                            <li>Using HTML</li>
+                                            <li>Using CSS</li>
+                                            <li>By React</li>
+                                        </ul>
+                                </div>
+                        </div>
                 
 
-            <div className='projectBar'>
-                <img src={project2} alt="project2" className="projectImg" />
-                <div>
-                    <h5>Sportify App | Java | <a href="https://github.com/HuyTran72/Sportify_App"> GitHub</a></h5>
-                    <ul>
-                        <li>Build an app music</li>
-                        <li>Play/ Stop/ Next/ Previous Song</li>
-                        <li>Show the list when finding</li>
-                    </ul>
-                </div> 
-            </div>
+                        <div className='projectBar'>
+                            <img src={project2} alt="project2" className="projectImg" />
+                            <div>
+                                <h5>Sportify App | Java | <a href="https://github.com/HuyTran72/Sportify_App"> GitHub</a></h5>
+                                <ul>
+                                    <li>Implemented a Sportify App that gathers data from the available list and developed a inteface using Java</li>
+                                    <li>The app supports searching the artist, song, and good communicating with functional buttons like play, stop, next, previous</li>
+                                </ul>
+                            </div> 
+                        </div>
                  
             
-            <div className='projectBar'>
-                <img src={project3} alt="project3" className="projectImg" />
-                <div>
-                    <h5>Egyptian Pyramids App | Java | <a href="https://github.com/HuyTran72/Nassef_Pyramids_App"> GitHub</a></h5>
-                    <ul>
-                        <li>Use Java to build Pyramids includes the list of Paraong's information</li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>  
-            </div>
+                        <div className='projectBar'>
+                            <img src={project3} alt="project3" className="projectImg" />
+                            <div>
+                                <h5>Egyptian Pyramids App | Java | <a href="https://github.com/HuyTran72/Nassef_Pyramids_App"> GitHub</a></h5>
+                                <ul>
+                                    <li>Use Java to build Pyramids includes the list of Paraong's information</li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>  
+                        </div>
                     
-        </div>
-    </section>
+                    </div>
+                </section>
             </Col>
         </Row>
     </Container>
